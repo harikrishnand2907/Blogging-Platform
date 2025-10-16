09b297f37362ef55eb2775aa061b852a6c9c8082
Hari krishnan <harikrishnand2907@gmail.com>
2025-10-16 22:27:25 +0530

    User Authentication and UI changes in Login and Sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller? && (controller_name == 'sessions' || controller_name == 'registrations')
+      'auth'
+    else
+      'application'
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    posts_path # Redirect to blog page after login
+  end
+
+  def after_sign_up_path_for(resource)
+    posts_path # Redirect to blog page after signup
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # Redirect to login page after logout
+  end
 end
